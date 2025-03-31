a4c07b5983d93effcded3c054cc8375d717ee51b
Giovanni Branco <giovannipbranco1@gmail.com>
2025-03-31 18:09:17 -0300

    Revert "feat: adding initial structure and first endpoints"
    This reverts commit 9650c8c347b9988682bba727cb0225a467d65dd0.

--- src/modules/user/interfaces/IUserService.ts
+++
@@ -1,8 +0,0 @@
-import { GetAllUsersInput } from '../dtos/inputs/getAllUsers.input';
-import { User } from '../entities/user';
-
-export const UserServiceToken = Symbol('UserService');
-export interface IUserService {
-  getAll(input: GetAllUsersInput): Promise<User[]>;
-  getById(id: number): Promise<User | null>;
-}
